30863e072fc6785357f32c12aa746d5007da57d0
Luan Lima <luan@yata.dev>
2021-05-12 09:20:40 -0300

    :sparkles:  Desafio Concluido

---
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,19 @@
+import { Response, Request } from "express";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+class CreateUserController {
+  constructor(private createUserUseCase: CreateUserUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    try {
+      const { email, name } = request.body;
+      const user = this.createUserUseCase.execute({ email, name });
+      return response.status(201).json(user);
+    } catch (e) {
+      return response.status(400).json({ error: e });
+    }
+  }
+}
+
+export { CreateUserController };
---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    try {
+      const { user_id } = request.headers;
+
+      const users = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+      return response.json(users);
+    } catch (e) {
+      return response.status(400).json({ error: e });
+    }
+  }
+}
+
+export { ListAllUsersController };
